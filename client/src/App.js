faae21df6a7629d115e42dae35930f86aebf00a4
Abhinav_Jha <abhi.mount.26@gmail.com>
2022-05-20 00:16:32 +0530

    pracel updated to v2

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import TextEditor from './Components/TextEditor'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import { v4 as uuId } from "uuid"
+import { v4 as uuId } from 'uuid'
 import Error from './Components/Error'
 const App = () => {
-   return (
-      <Router>
-         <Switch>
-            <Route path='/' exact>
-               <Redirect to={`/documents/${uuId()}`} />
-            </Route>
-            <Route path='/documents/:id'>
-               <TextEditor />
-            </Route>
-            <Route to='*'>
-               <Error />
-            </Route>
-         </Switch>
-      </Router>
-   )
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to={`/documents/${uuId()}`} />
+        </Route>
+        <Route path='/documents/:id'>
+          <TextEditor />
+        </Route>
+        <Route to='*'>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
